1af9cc1b9456f078b31490c8311155bdddd2b6e0
luiscardona1997 <cardona7daniel@gmail.com>
2022-06-01 18:23:21 -0500

    github folder

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   const configService = app.get(ConfigService);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
